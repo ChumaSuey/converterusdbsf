09daf1b6fd9cf8c75a4c8c94fa15df88a0fe7b73
ChumaSuey <hyperghostblade@gmail.com>
2023-04-27 12:14:24 -0400

    .

--- Converter/settings.py
+++ Converter/settings.py
@@ -22,8 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_4^mw3)=dey0el%48r0e=(c63q^m=3o=ue60iq6lb$di-02t@y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
