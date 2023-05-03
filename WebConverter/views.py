349bbff03b44979d4cfac64e9c291924b44f2fcb
ChumaSuey <hyperghostblade@gmail.com>
2023-05-03 11:06:12 -0400

    .

--- WebConverter/views.py
+++ WebConverter/views.py
@@ -6,21 +6,29 @@ import requests
 
 
 def index(request: HttpRequest):
-    context = {"dolar_price": "", "dollars": "", "bols":""}
+    context = {"dolar_price": "", "dollars": "", "bolivares": "", "bolivares_price" : ""}
     if request.method == "POST":
         dollars = int(request.POST.get('dolar', 0))
         response = requests.get("https://s3.amazonaws.com/dolartoday/data.json")
         dolar_price = response.json()['USD']['dolartoday']
         context["dolar_price"] = float(dolar_price) * dollars
         context["dollars"] = dollars
-        context["bols"] = float(dolar_price) / dolar_price
+    elif 'bolivares' in request.POST:
+        bolivares = int(request.POST.get('bolivares', 0))
+        response = requests.get("https://s3.amazonaws.com/dolartoday/data.json")
+        dolar_price = response.json()['USD']['dolartoday']
+        context["bolivares_price"] = bolivares / float(dolar_price)
+        context["bolivares"] = bolivares
+        context["dollars"] = ""
+       # context["dolares"] = "bolivares"
+
     template = 'index.html'
     return render(request = request, template_name=template, context=context)
 
 
 
-
-
+# context["bolivares"] = bolivares
+# context["bolivares"] = bolivares / float(dolar_price)
 
 #def currency():
  #   """ Data de Dolartoday  """
